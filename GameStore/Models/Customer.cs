59002ccfaf4d1d5bea369131b0f5472b4789eb74
JorgenMJobloop <jorgen@jobloop.no>
2026-04-23 14:42:18 +0200

    Delete GameStore directory

--- GameStore/Models/Customer.cs
+++
@@ -1,9 +0,0 @@
-namespace GameStore.Api.Models;
-
-public class Customer
-{
-    public int Id { get; set; }
-    public string FirstName { get; set; } = string.Empty;
-    public string EmailAddress { get; set; } = string.Empty;
-    public ICollection<Order> Orders { get; set; } = new List<Order>();
-}
--- GameStore/README.md
+++
@@ -1,7 +0,0 @@
-# GameStore
-
-## A mock webstore/webshop
-a mock webshop for gaming consoles, PC-gaming, videogame titles and computer hardware
-
-### Graphics
-![alt text](image.png)
